78d6350a6efb7f70ad897830b34051fb5ba8413c
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-10-21 12:21:48 -0600

    Fixed bug where agent would keep playing while dead

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pettingzoo.sisl.waterworld_v4
+
+
 # Link to environment base class:
 # from pettingzoo.sisl.waterworld.waterworld_base import WaterworldBase
+
 
 @property
 def base_env(self) -> pettingzoo.sisl.waterworld_v4.env:
@@ -8,15 +11,16 @@ def base_env(self) -> pettingzoo.sisl.waterworld_v4.env:
 
 
 if __name__ == "__main__":
-    env = pettingzoo.sisl.waterworld_v4.env(render_mode="human", FPS=120)
-    setattr(env.__class__, 'base_env', base_env)
+    kwargs = dict(render_mode="human", FPS=120, n_poisons=100)
+    env = pettingzoo.sisl.waterworld_v4.env(**kwargs)
+    setattr(env.__class__, "base_env", base_env)
     print(f"Running at {env.base_env.FPS} FPS")
     env.reset()
     env.render()
 
     for agent in env.agent_iter():
         obs, reward, done, truncated, info = env.last()
-        action = None if env.terminations[agent] else env.action_space(agent).sample()
+        action = None if env.terminations[agent] or env.truncations[agent] else env.action_space(agent).sample()
         env.step(action)
         env.render()
     env.close()

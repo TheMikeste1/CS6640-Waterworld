5d063106352440e67af74c8456213a5b7ba6dfe8
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-10-21 12:08:50 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import pettingzoo.sisl.waterworld_v4
+
+
+@property
+def base_env(self) -> pettingzoo.sisl.waterworld_v4.env:
+    return self.env.env.env
+
+
+if __name__ == "__main__":
+    env = pettingzoo.sisl.waterworld_v4.env(render_mode="human", FPS=120)
+    setattr(env.__class__, 'base_env', base_env)
+    print(f"Running at {env.base_env.FPS} FPS")
+    env.reset()
+    env.render()
+
+    for agent in env.agent_iter():
+        obs, reward, done, truncated, info = env.last()
+        action = None if env.terminations[agent] else env.action_space(agent).sample()
+        env.step(action)
+        env.render()
+    env.close()

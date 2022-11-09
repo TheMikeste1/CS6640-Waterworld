f3e1583bf47b5975eb93d799e33b3c6b4e0dd55f
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-11-09 11:18:49 -0700

    DistanceNN now works properly

--- agents/DistanceNeuralNetwork.py
+++ agents/DistanceNeuralNetwork.py
@@ -7,45 +7,31 @@ class DistanceNeuralNetwork(NeuralNetwork):
     def __init__(
         self,
         layers: [torch.nn.Module],
-        poison_layers: [torch.nn.Module],
-        pursuer_layers: [torch.nn.Module],
-        food_layers: [torch.nn.Module],
-        obstacle_layers: [torch.nn.Module],
-        barrier_layers: [torch.nn.Module],
+        distance_layers: [torch.nn.Module],
         speed_features: bool,
-        num_sensors: int = None,
+        num_sensors: int,
     ):
         """
         If num_sensors is None, the model will try to calculate the number of layers
         itself.
         """
         super().__init__(layers)
-        self.poison_layers = NeuralNetwork(layers=poison_layers)
-        self.pursuer_layers = NeuralNetwork(layers=pursuer_layers)
-        self.food_layers = NeuralNetwork(layers=food_layers)
-        self.obstacle_layers = NeuralNetwork(layers=obstacle_layers)
-        self.barrier_layers = NeuralNetwork(layers=barrier_layers)
-        # All incoming layers should all have the same number of inputs
-        assert (
-            self.poison_layers.in_features == self.pursuer_layers.in_features
-            and self.poison_layers.in_features == self.food_layers.in_features
-        ), (
-            "Poison, Pursuer, Food, Barrier, and Obstacle layers should all have"
-            "the same number of inputs: one for each input sensor."
+        self.distance_layers = NeuralNetwork(layers=distance_layers)
+        # Distance layers should have 5 incoming channels--one for each
+        # distance feature type:
+        #   obstacle, barrier, food, poison, pursuer
+        assert self.distance_layers.in_features == 5, (
+            f"Distance layers should have 5 incoming channels,"
+            f"but found {self.distance_layers.in_features}"
         )
-        self.num_sensors = (
-            num_sensors if num_sensors else self.poison_layers.in_features
-        )
+
+        self.num_sensors = num_sensors
         self.speed_features = speed_features
 
         # The number of inputs to layers must be the sum of outputs
-        # for all distance layers, plus all speed features, plus 2 for collisions
+        # for all 5 distance channels, plus all speed features, plus 2 for collisions
         total_layer_outputs = (
-            self.poison_layers.out_features
-            + self.pursuer_layers.out_features
-            + self.food_layers.out_features
-            + self.obstacle_layers.out_features
-            + self.barrier_layers.out_features
+            self.distance_layers.out_features * num_sensors
             + (self.num_sensors * 3 if self.speed_features else 0)
             + 2
         )
@@ -61,6 +47,34 @@ class DistanceNeuralNetwork(NeuralNetwork):
         return self.num_sensors * (5 + (3 if self.speed_features else 0)) + 2
 
     def forward(self, obs: torch.Tensor) -> torch.Tensor:
+        distance_obs, speed_obs, collision_obs = self.slice_features(obs)
+
+        # Process distance features
+        distance_obs = self.distance_layers(distance_obs)
+        # Flatten distance features for use with other features
+        distance_obs = distance_obs.flatten(start_dim=-2)
+        distance_obs = distance_obs.unsqueeze(dim=-2)
+
+        # Flatten speed features for use with other features
+        speed_obs = speed_obs.flatten(start_dim=-2)
+        speed_obs = speed_obs.unsqueeze(dim=-2)
+
+        # Concatenate all observations
+        obs = torch.cat(
+            [
+                distance_obs,
+                speed_obs,
+                collision_obs,
+            ],
+            dim=-1,
+        )
+
+        # Run observations through the main layers
+        return super(DistanceNeuralNetwork, self).forward(obs)
+
+    def slice_features(
+        self, obs: torch.Tensor
+    ) -> (torch.Tensor, torch.Tensor, torch.Tensor):
         current_slice = 0
         # Obstacle features
         obstacle_distance_obs = obs[
@@ -106,13 +120,7 @@ class DistanceNeuralNetwork(NeuralNetwork):
             current_slice == obs.shape[-1]
         ), f"Too many observations! Found {obs.shape[-1]} but expected {current_slice}"
 
-        # Run observations through each layer
-        obstacle_distance_obs = self.obstacle_layers(obstacle_distance_obs)
-        barrier_distance_obs = self.barrier_layers(barrier_distance_obs)
-        food_distance_obs = self.food_layers(food_distance_obs)
-        poison_distance_obs = self.poison_layers(poison_distance_obs)
-        pursuer_distance_obs = self.pursuer_layers(pursuer_distance_obs)
-        # Concatenate all distance observations
+        # Stack all distance features
         distance_obs = torch.cat(
             [
                 obstacle_distance_obs,
@@ -121,25 +129,16 @@ class DistanceNeuralNetwork(NeuralNetwork):
                 poison_distance_obs,
                 pursuer_distance_obs,
             ],
-            dim=-1,
+            dim=-2,
         )
-        # Concatenate all speed observations
+        # Stack all speed observations
         speed_obs = torch.cat(
             [
                 food_speed_obs,
                 poison_speed_obs,
                 pursuer_speed_obs,
             ],
-            dim=-1,
+            dim=-2,
         )
-        # Concatenate all observations
-        obs = torch.cat(
-            [
-                distance_obs,
-                speed_obs,
-                collision_obs,
-            ],
-            dim=-1,
-        )
-        # Run observations through the main layers
-        return super(DistanceNeuralNetwork, self).forward(obs)
+
+        return distance_obs, speed_obs, collision_obs
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from torch.utils.tensorboard import SummaryWriter
 
 import custom_waterworld
 from agents import NeuralNetwork, QNNAgent
+from agents.DistanceNeuralNetwork import DistanceNeuralNetwork
 from custom_waterworld import WaterworldArguments
 
 
@@ -34,18 +35,36 @@ def main():
     env = waterworld.env(**args.to_dict())
 
     num_obs = env.observation_space(env.possible_agents[0]).shape[0]
-
+    num_sensors = args.n_sensors
     # Create agents
-    agent_name = "qnn_simple_linear_256_64_3"
+    agent_name = "qnn_distance"
     policy_networks = [
-        NeuralNetwork(
+        DistanceNeuralNetwork(
             layers=[
-                torch.nn.Linear(num_obs, 256),
+                # out_channels * num_sensors + 2 collision features + 3 speed layers
+                torch.nn.Linear(32 * num_sensors + 2 + num_sensors * 3, 256),
                 torch.nn.ReLU(),
-                torch.nn.Linear(256, 64),
+                torch.nn.Linear(256, 256),
                 torch.nn.ReLU(),
-                torch.nn.Linear(64, 3),
+                torch.nn.Linear(256, 3),
             ],
+            distance_layers=[
+                torch.nn.Conv1d(
+                    in_channels=5,
+                    out_channels=32,
+                    kernel_size=3,
+                    padding=1,
+                ),
+                torch.nn.ReLU(),
+                torch.nn.Conv1d(
+                    in_channels=32,
+                    out_channels=32,
+                    kernel_size=3,
+                    padding=1,
+                ),
+            ],
+            speed_features=True,
+            num_sensors=num_sensors,
         )
         for _ in range(2)
     ]

98873f8348907725af5e945f31640831770be257
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-11-21 17:56:00 -0700

    Added RunArguments Builder

--- run_arguments.py
+++ run_arguments.py
@@ -12,6 +12,56 @@ if TYPE_CHECKING:
 
 @dataclass(frozen=True, kw_only=True, slots=True)
 class RunArguments:
+    @dataclass
+    class Builder:
+        agent_builders: List[AgentBuilder] | AgentBuilder
+        environment_args: WaterworldArguments
+        num_episodes: int
+        run_name: str
+        should_record: bool
+
+        def add_agent_builder(
+            self, agent_builder: AgentBuilder
+        ) -> RunArguments.Builder:
+            if isinstance(self.agent_builders, list):
+                self.agent_builders.append(agent_builder)
+            else:
+                self.agent_builders = [self.agent_builders, agent_builder]
+            return self
+
+        def set_agent_builders(
+            self, agent_builders: Iterable[AgentBuilder]
+        ) -> RunArguments.Builder:
+            self.agent_builders = list(agent_builders)
+            return self
+
+        def set_environment_args(
+            self, environment_args: WaterworldArguments
+        ) -> RunArguments.Builder:
+            self.environment_args = environment_args
+            return self
+
+        def set_num_episodes(self, num_episodes: int) -> RunArguments.Builder:
+            self.num_episodes = num_episodes
+            return self
+
+        def set_run_name(self, run_name: str) -> RunArguments.Builder:
+            self.run_name = run_name
+            return self
+
+        def set_should_record(self, should_record: bool) -> RunArguments.Builder:
+            self.should_record = should_record
+            return self
+
+        def build(self) -> RunArguments:
+            return RunArguments(
+                agent_builders=self.agent_builders,
+                environment_args=self.environment_args,
+                num_episodes=self.num_episodes,
+                run_name=self.run_name,
+                should_record=self.should_record,
+            )
+
     agent_builders: List[AgentBuilder] | AgentBuilder
     environment_args: WaterworldArguments
     num_episodes: int
